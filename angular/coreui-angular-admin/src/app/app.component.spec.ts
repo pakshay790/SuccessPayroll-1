7940d751c281e9382deca6ebfe0ef37733bd5e83
pakshay790 <paksahy790@gmail.com>
2022-01-27 16:39:59 +0530

    New Branch created

--- angular/coreui-angular-admin/src/app/app.component.spec.ts
+++ angular/coreui-angular-admin/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CoreUI Free Angular Admin Template'`, () => {
+  it(`should have as title 'Employee Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('CoreUI Free Angular Admin Template');
