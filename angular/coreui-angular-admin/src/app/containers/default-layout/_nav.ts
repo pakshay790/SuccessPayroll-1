07a5e1ed9088a98930736fb7411c58e44722dfca
pakshay790 <paksahy790@gmail.com>
2022-02-03 12:21:58 +0530

    Payroll added to the left nav

--- angular/coreui-angular-admin/src/app/containers/default-layout/_nav.ts
+++ angular/coreui-angular-admin/src/app/containers/default-layout/_nav.ts
@@ -2,13 +2,6 @@ import { INavData } from '@coreui/angular';
 
 export const navItems: INavData[] = [
   {
-    
-    name: 'Dashboard',
-    url: '/dashboard',
-    iconComponent: { name: 'cil-speedometer' },
-    
-  },
-  {
     title: true,
     name: 'Accounting',
   },
