3bd86433e33b658e5a5ab83d4e37301f1bead5e6
Ann <annmerlyn31@gmail.com>
2022-02-04 16:41:38 +0530

    added my team management page, moved dashboard contents to it, added the navigation aswell

--- angular/coreui-angular-admin/src/app/app.module.ts
+++ angular/coreui-angular-admin/src/app/app.module.ts
@@ -43,6 +43,7 @@ import {
   SidebarModule,
   TabsModule,
   UtilitiesModule,
+  WidgetModule,
 } from '@coreui/angular';
 
 import { IconModule, IconSetService } from '@coreui/icons-angular';
@@ -94,6 +95,7 @@ const APP_CONTAINERS = [
     ListGroupModule,
     CardModule,
     HttpClientModule,
+    WidgetModule,
   ],
   providers: [
     {
